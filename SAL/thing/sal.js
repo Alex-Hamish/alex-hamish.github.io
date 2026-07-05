122af31476afec2f3761054937ae12cfa3666365
Alex-Hamish <125255039+Alex-Hamish@users.noreply.github.com>
2026-07-05 05:29:50 +0000

    added nodejs ansi-to-html and added ansi functionality

--- SAL/thing/sal.js
+++ SAL/thing/sal.js
@@ -1,14 +1,17 @@
+var Convert = require('ansi-to-html');
 const consolearea = document.getElementById("Crea");
 
 function AddText(strg){
-    a = document.createElement("p");
-    a.innerHTML = strg;
-    consolearea.append(a)
+    var convert = new Convert();
+    f = convert.toHtml(strg); // Convert ANSI to HTML
+    // some DOM shit
+    console.log(f);    
 }
 
 function LOAD(){
     // Start reading stuff
-    AddText("LD-0")
+    document.getElementById("ld1").remove(); // Remove that button !!!
+    AddText("ST-LOAD");
 }
 
 document.getElementById("ld1").addEventListener("click", LOAD());
