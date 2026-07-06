a7ca0fb567b589dfb99e310dfdc3f2965839681e
Alex-Hamish <125255039+Alex-Hamish@users.noreply.github.com>
2026-07-06 04:31:42 +0000

    added getflags function

--- SAL/thing/sal.js
+++ SAL/thing/sal.js
@@ -11,6 +11,35 @@ function AddText(strg){
 
 AddText("Starting Load...");
 
+AddText("\nStarting Load:\x1b[38;5;91m important functions \x1b[0m");
+function getflags(strg){
+    opes = strg.split(' ');
+    opes.splice(0);
+    let a = [];
+    let j = 0;
+    if (opes.length == 0){
+        return a;
+    }
+    for (const verr of opes) {
+        if (verr[0] == "-") {
+            let derr = verr.substr(1);
+            for (char in derr){
+                a.push(char)
+            }
+        }
+        if (verr[1] == "-") {
+            let derr = verr.substr(2);
+            a.push(derr)
+        }
+        j++;
+    }
+    return a; 
+}
+AddText("\nLoaded\x1b[38;5;91m getflags \x1b[0m");
+
+
+
+
 AddText("\nStarting Load:\x1b[38;5;91m cmds \x1b[0m");
 
 function comm(strg){
@@ -23,6 +52,7 @@ function comm(strg){
     }
 }
 
+AddText("\nDone Load:\x1b[38;5;91m cmds \x1b[0m");
 
 AddText("\nStarting Load:\x1b[38;5;91m input \x1b[0m");
 
