5e62042523eb7298b23f5a36ab23c3e92b773aff
Alex-Hamish <125255039+Alex-Hamish@users.noreply.github.com>
2026-07-15 11:59:02 +0000

    warning about warnings and new line also new commands and files

--- SAL/thing/sal.js
+++ SAL/thing/sal.js
@@ -4,6 +4,33 @@ const car = document.getElementById("output");
 let inp = "";
 let currtext = "";
 let files = {};
+let examplefile = {
+    "name": "example.exa", // the file ext exa is a custom file extension. it can be up to 4 chars.
+    "magicword": "!exa-1!", // because this is a file, it has a magicword. this is used to identify the file in the files object. it can be anything, but it should be unique.
+    "content": "This is an example file." // exa is a txt confirmed.
+}
+let examplefile2 = {
+    "name": "zipfile.zip",
+    "magicword": "!zip!", // zip files are folder files. unzip literally just changes the magic word and the name.
+    "content": {
+        "file1.txt": {
+            "name": "file1.txt",
+            "magicword": "!txt!", // it's often just the file extension, but it can be anything. just make sure it's unique.
+            "content": "This is a text file inside a zip file to show that SAL can handle zip files."
+        },
+        "folder": {
+            "name": "folder",
+            "magicword": "!f!", // FOLDER! (folder magicword)
+            "content": {
+                "file2.txt": {
+                    "name": "file2.txt",
+                    "magicword": "!txt2!",
+                    "content": "This is a text file inside a folder inside a zip file to show that SAL can handle zip files."
+                }
+            }
+        }
+    }
+}
 
 let dict = {
     OSType: "Yek", // Yek... hamnurber.
@@ -25,11 +52,26 @@ function load(){
     let loadedFiles = localStorage.getItem("files");
     if (loadedFiles) {
         files = JSON.parse(loadedFiles);
+    } else {
+        files[examplefile.magicword] = examplefile;
+        files[examplefile2.magicword] = examplefile2;
     }
     if (loaded) {
         dict = JSON.parse(loaded);
     }
 }
+
+function waitForKey() {
+    return new Promise(resolve => {
+        function onKey(e) {
+            document.removeEventListener("keydown", onKey);
+            resolve(e.key); // Returns the key that was pressed
+        }
+
+        document.addEventListener("keydown", onKey);
+    });
+}
+
 
 AddText("Starting Load...");
 
@@ -128,6 +170,13 @@ function getarg(strg, pos){ // pos is the position (starting from 0), so if you 
 
 AddText("\nLoaded\x1b[38;5;82m getarg \x1b[0m");
 
+function yesno(){
+    // this is a thingy that gets y/n prompts
+    return waitForKey().then(key => {
+        return key === "y" || key === "Y";
+    });
+}
+
 AddText("\nLoaded\x1b[38;5;82m important functions \x1b[0m");
 
 AddText("\nStarting Load:\x1b[38;5;82m cmds \x1b[0m");
@@ -161,6 +210,13 @@ You can also use "variables" to see all variables.
             AddText("clear: Clears the terminal.\n");
             AddText("variables: Displays all variables.\n");
             AddText("rmvar: Removes a variable. Usage: rmvar <variable_name>\n");
+            AddText("save: Saves the current variables and files to localStorage.\n");
+            AddText("load: Loads the variables and files from localStorage.\n");
+            AddText("loadbackup: Loads a backup of the current variables and files from a JSON file.\n");
+            AddText("cat: Displays the content of a file. Usage: cat <file_name>\n");
+            AddText("ls: Lists all files.\n");
+            AddText("unzip: Unzips a zip file into a folder. Usage: unzip <zip_file_name>\n");
+            AddText("zip: Zips a folder into a zip file. Usage: zip <folder_name>\n");
             AddText("backup: Creates a backup of the current variables and files.\n");
             AddText("man: Displays the manual.\n");
             AddText("help: Displays this help message.\n");
@@ -177,6 +233,118 @@ You can also use "variables" to see all variables.
                 AddText("Variable not found: " + getarg(strg, 1) + "\n");
             }
             break;
+        case "save":
+            save();
+            AddText("Saved variables and files to localStorage.\n");
+            break;
+        case "load":
+            load();
+            AddText("Loaded variables and files from localStorage.\n");
+            break;
+        case "loadbackup":
+            let inputElement = document.createElement('input');
+            inputElement.type = 'file';
+            inputElement.accept = '.json';
+            inputElement.addEventListener('change', (event) => {
+                const file = event.target.files[0];
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    try {
+                        const backupData = JSON.parse(e.target.result);
+                        if (backupData.dict && backupData.files) {
+                            dict = backupData.dict;
+                            files = backupData.files;
+                            AddText("Backup loaded successfully.\n");
+                        } else {
+                            AddText("Invalid backup file format.\n");
+                        }
+                    } catch (error) {
+                        AddText("Error loading backup: " + error.message + "\n");
+                    }
+                };
+                reader.readAsText(file);
+            });
+            inputElement.click();
+            break;
+        case "cat":
+            let fileToCat = getarg(strg, 1);
+            let found = false;
+            for (const [key, file] of Object.entries(files)) {
+                if (file.name === fileToCat) {
+                    if (file.magicword === "!zip!" || file.magicword === "!f!") {
+                        AddText("Cannot cat a folder or zip file. Use unzip or navigate into the folder.\n");
+                    } else {
+                        AddText(file.content + "\n");
+                    }
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                AddText("File not found: " + fileToCat + "\n");
+            }
+            break;
+        case "ls":
+            for (const [key, file] of Object.entries(files)) {
+                AddText(file.name + "\n");
+            }
+            break;
+        case "unzip":
+            let fileToUnzip = getarg(strg, 1);
+            let foundUnzip = false;
+            for (const [key, file] of Object.entries(files)) {
+                if (file.name === fileToUnzip) {
+                    if (file.magicword === "!zip!") {
+                        files[file.magicword] = "!f!"; // change magicword to folder
+                        files[file.name] = file.slice(0, -4); // remove .zip from name
+                        AddText("Unzipped " + fileToUnzip + " into a folder.\n");
+                    } else {
+                        AddText("File is not a zip file: " + fileToUnzip + "\n");
+                    }
+                    foundUnzip = true;
+                    break;
+                }
+            }
+            if (!foundUnzip) {
+                AddText("File not found: " + fileToUnzip + "\n");
+            }
+            break;
+        case "zip":
+            let fileToZip = getarg(strg, 1);
+            let foundZip = false;
+            for (const [key, file] of Object.entries(files)) {
+                if (file.name === fileToZip) {
+                    if (file.magicword === "!f!") {
+                        files[file.magicword] = "!zip!"; // change magicword to zip
+                        files[file.name] = file.name + ".zip"; // add .zip to name
+                        AddText("Zipped " + fileToZip + " into a zip file.\n");
+                    } else {
+                        AddText("File is not a folder: " + fileToZip + "\n");
+                    }
+                    foundZip = true;
+                    break;
+                }
+            }   
+            if (!foundZip) {
+                AddText("File not found: " + fileToZip + "\n");
+            }
+            break;
+        case "debug":
+            if (getarg(strg, 1) == "#0[[FILE]]") {
+                AddText("Are you sure? This will add the example files. (y/n)\n");
+                yesno().then(yn => {
+                    if (yn) {
+                        files[examplefile.magicword] = examplefile;
+                        files[examplefile2.magicword] = examplefile2;
+                        AddText("Example files added.\n");
+                    } else {
+                        AddText("Operation cancelled.\n");
+                    }
+                });
+
+            }
+
+
         case "backup":
             let dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(dict));
             let downloadAnchorNode = document.createElement('a');
@@ -250,5 +418,9 @@ input.addEventListener("keydown", e => {
 
 AddText("\n\x1b[38;5;196mWARNING: This is a very early version of SAL. It is not stable and may have bugs. Please report any issues to the developer.\x1b[0m");
 AddText("\n\x1b[38;5;196mWARNING: Your browser may delete your files and folders. Please backup your data by using <em>backup</em> before using this terminal.\x1b[0m");
+AddText("\n\x1b[38;5;196mWARNING: These warnings warn you about potential risks other than this warning that warns you about warnings warning you about risks (say that five times fast).\x1b[0m");
+
+AddText("\n"); // because of the warnings, we need a new line before the input
+
 
 window.addEventListener("beforeunload", save);
