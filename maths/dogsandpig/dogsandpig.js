73b88c2d7962565833cd3d85285cc6e4d4d61a1f
Alex-Hamish <125255039+Alex-Hamish@users.noreply.github.com>
2026-08-04 11:17:25 +0000

    compacted the thingies

--- maths/dogsandpig/dogsandpig.js
+++ maths/dogsandpig/dogsandpig.js
@@ -90,18 +90,17 @@ function getMoves(pos) {
 function drawBoard() {
     const canvas = document.getElementById("game-canvas");
     const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, 600, 100);
 
     const positions = [
         { x: 300, y: 50 },
         { x: 150, y: 150 },
         { x: 300, y: 150 },
         { x: 450, y: 150 },
-        { x: 100, y: 300 },
+        { x: 150, y: 300 },
         { x: 300, y: 300 },
-        { x: 500, y: 300 },
+        { x: 450, y: 300 },
         { x: 150, y: 450 },
         { x: 300, y: 450 },
         { x: 450, y: 450 },
