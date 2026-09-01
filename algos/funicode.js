4ea5c36a1917606018c3f60aa3a9d0a2a8900155
Aex <127460@student.bmg.vic.edu.au>
2026-09-01 06:43:43 +0000

    changed some stuff with funicode so the image isn't smoothed

--- algos/funicode.js
+++ algos/funicode.js
@@ -26,68 +26,11 @@ function makefunicode(text){
     canvas.width = 32;
     canvas.height = 32;
 
-
-    // Create a blank 32x32 image
-    const img = ctx.createImageData(32, 32);
-
-    // Set a pixel
-    function setPixel(x, y, bit) {
-        const i = (y * 32 + x) * 4;
-        let a = 255;
-        switch (bit){
-            case 0:
-                img.data[i]     = 0;
-                img.data[i + 1] = 0;
-                img.data[i + 2] = 0;
-                img.data[i + 3] = a;
-                break;
-            case 1:
-                img.data[i]     = a;
-                img.data[i + 1] = a;
-                img.data[i + 2] = a;
-                img.data[i + 3] = a;
-                break;
-            case 2:
-                img.data[i]     = a;
-                img.data[i + 1] = 0;
-                img.data[i + 2] = 0;
-                img.data[i + 3] = a;
-                break;
-            case 3:
-                img.data[i]     = 0;
-                img.data[i + 1] = a;
-                img.data[i + 2] = 0;
-                img.data[i + 3] = a;
-                break;
-            case 4:
-                img.data[i]     = a;
-                img.data[i + 1] = a;
-                img.data[i + 2] = 0;
-                img.data[i + 3] = a;
-                break;
-            case 5:
-                img.data[i]     = 0;
-                img.data[i + 1] = 0;
-                img.data[i + 2] = a;
-                img.data[i + 3] = a;
-                break;
-            case 6:
-                img.data[i]     = a;
-                img.data[i + 1] = 0;
-                img.data[i + 2] = a;
-                img.data[i + 3] = a;
-                break;
-            case 7:
-                img.data[i]     = 0;
-                img.data[i + 1] = a;
-                img.data[i + 2] = a;
-                img.data[i + 3] = a;
-                break;
-        }
-    }
     ctx.lineWidth = 1;
+    ctx.strokeStyle = "white";
     ctx.strokeRect(1, 1, 6, 6);
-    ctx.strokeRect(3, 3, 4, 4);
+
+
 
     console.log("added thingy")
     // Display it
