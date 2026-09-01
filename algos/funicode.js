d5390d88b0f1c485a9b54efcd177f3d85d246f24
Aex <127460@student.bmg.vic.edu.au>
2026-09-01 06:52:08 +0000

    changed size

--- algos/funicode.js
+++ algos/funicode.js
@@ -32,7 +32,7 @@ function makefunicode(text){
     // Draw the funicode
     ctx.lineWidth = 1;
     ctx.strokeStyle = "white";
-    ctx.strokeRect(1, 1, 6, 6);
+    ctx.strokeRect(2, 2, 7, 7);
 
 
 
